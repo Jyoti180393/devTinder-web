cd2622e9ce0ad53a0f8604546df5691fab8310ab
Jyoti180393 <pokhariajyoti18@gmail.com>
2026-06-15 14:33:59 +0530

    Added Tailwind using Vite

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ function App() {
   return (
     <>
       <section id="center">
-          <h1>Hello world</h1>
+       <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
       </section>
 
     </>
